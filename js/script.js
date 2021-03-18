c387ba2995b102df8d10d1b4c921da2079df0108
GioOli7 <gio07live@gmail.com>
2021-03-18 15:20:31 +0100

    Base logic js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-console.log("TEST")
+/**************************************
+ * 
+ * GENRATORE DI PASSWORD NON SICURE
+ * 
+ * ********************************* */ 
+
+/* Steps
+
+1 - Chiedi nome
+2 - Chiedi Cognome
+3 - Chiedi colore preferito
+4 - Concatena le tre stinghe, + una quarta con stringa "21"
+5 - Stampa la "password" generata nella pagina HTML
+*/
+
+var nome = prompt("Qual è il tuo nome?");
+console.log(nome);
+console.log( typeof(nome) );
+
+var cognome = prompt("Qual è il tuo cognome?");
+console.log(cognome);
+console.log( typeof(cognome) );
+
+var colore = prompt("Qual è il tuo colore preferito?");
+console.log(colore);
+console.log( typeof(colore) );
+
+var stringBonus = "21";
+
+var password = nome + cognome + colore + stringBonus;
+console.log(password);
+console.log( typeof(password) );
+
+document.getElementById("display_password").innerHTML = password;
